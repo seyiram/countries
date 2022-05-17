d3a14592159c5be48948c432b15baa98e85cce9e
seyiram <akanyi.king@gmail.com>
2022-05-17 01:00:18 -0400

    add countries cards

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,70 @@
-
+import germanFlag from "../img/Flag_of_Germany.svg";
 
 const Countries = () => {
   return (
-    <div>Countries</div>
-  )
-}
+    <div className="countries">
+      <div className="countries__card">
+        <img src={germanFlag} alt="flag" />
+        <div className="countries__card--content">
+          <h4>Germany</h4>
+          <p>
+            <span>Population: </span> 89,000,000
+          </p>
+          <p>
+            <span>Region: </span> Europe
+          </p>
+          <p>
+            <span>Capital: </span> Berlin
+          </p>
+        </div>
+      </div>
+      <div className="countries__card">
+        <img src={germanFlag} alt="flag" />
+        <div className="countries__card--content">
+          <h4>Germany</h4>
+          <p>
+            <span>Population: </span> 89,000,000
+          </p>
+          <p>
+            <span>Region: </span> Europe
+          </p>
+          <p>
+            <span>Capital: </span> Berlin
+          </p>
+        </div>
+      </div>
+      <div className="countries__card">
+        <img src={germanFlag} alt="flag" />
+        <div className="countries__card--content">
+          <h4>Germany</h4>
+          <p>
+            <span>Population: </span> 89,000,000
+          </p>
+          <p>
+            <span>Region: </span> Europe
+          </p>
+          <p>
+            <span>Capital: </span> Berlin
+          </p>
+        </div>
+      </div>
+      <div className="countries__card">
+        <img src={germanFlag} alt="flag" />
+        <div className="countries__card--content">
+          <h4>Germany</h4>
+          <p>
+            <span>Population: </span> 89,000,000
+          </p>
+          <p>
+            <span>Region: </span> Europe
+          </p>
+          <p>
+            <span>Capital: </span> Berlin
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Countries
+export default Countries;
